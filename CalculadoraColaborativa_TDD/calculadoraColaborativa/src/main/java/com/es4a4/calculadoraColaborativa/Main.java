e788ea766ea618541bb89eb224c6aa3237b1e036
Gabriel Trindade <icandoitgrt@gmail.com>
2021-11-07 21:00:37 -0300

    Implementação do Main

--- CalculadoraColaborativa_TDD/calculadoraColaborativa/src/main/java/com/es4a4/calculadoraColaborativa/Main.java
+++ CalculadoraColaborativa_TDD/calculadoraColaborativa/src/main/java/com/es4a4/calculadoraColaborativa/Main.java
@@ -1,13 +1,60 @@
 package com.es4a4.calculadoraColaborativa;
 
 import java.util.List;
+import java.util.Scanner;
 
+import com.es4a4.calculadoraColaborativa.dweet.GetDweet;
+import com.es4a4.calculadoraColaborativa.dweet.PostDweet;
 import com.es4a4.calculadoraColaborativa.services.Operations;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		List<String> p = GetDweet.get("es4a4-fgmtem");
+		Integer num1 = 0;
+		Integer num2 = 0;
+		String opcao = "";
+		do {
+			Menu();
+			System.out.print("Digite uma opção => ");
+			opcao = input.next();
+
+			if (opcao.toLowerCase().equals("sair"))
+				break;
+
+			// aqui é uma espécie de verificar se alguma outra instância da calculadora fez
+			// algum post.
+			p = GetDweet.get("es4a4-fgmtem");
+			if (!(p.get(0).equals(opcao) && p.get(1).equals(num1.toString()) && p.get(2).equals(num2.toString()))) {
+				System.out.println("\n******************\n\tResultado de uma outra calculadora (instância):");
+				result(p);
+				System.out.println("\n******************");
+			}
+
+			System.out.print("\nDigite o 1º número: ");
+			num1 = input.nextInt();
+			System.out.print("Digite o 2º número: ");
+			num2 = input.nextInt();
+
+			// verificação para não dar erros futuramente
+			opcao = opcao.toLowerCase();
+			if (opcao.equals("somar") || opcao.equals("subtrair") || opcao.equals("multiplicar")
+					|| opcao.equals("dividir")) {
+				PostDweet.post(opcao.toLowerCase(), num1, num2);
+
+				// recuperar do dweet
+				p = GetDweet.get("es4a4-fgmtem");
+				result(p);
+
+			} else {
+				System.out.println("Tente novamente! Algum dado de entrada inválido.");
+			}
+
+		} while (!opcao.toLowerCase().equals("sair"));
+
+		input.close();
+		System.out.println("Programa Finalizado!");
 	}
 
 	public static void Menu() {
